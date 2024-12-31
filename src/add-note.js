1f18988b13bab1b3ebfa833da44eccb9680f9e26
Conut-1 <1mim1@naver.com>
2024-12-31 23:27:24 +0900

    feat: globalStorage에 입력 값 저장

--- src/add-note.js
+++ src/add-note.js
@@ -1,8 +1,17 @@
 import vscode from "./vscode-module.js";
+import { join } from "path";
+import { writeFile } from "fs/promises";
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-export function addNote(context) {
-  vscode.window.showInformationMessage("add note");
+export async function addNote(context) {
+  await vscode.workspace.fs.createDirectory(context.globalStorageUri);
+  const input = await vscode.window.showInputBox({
+    placeHolder: "Type your input here",
+    prompt: "Enter something:",
+  });
+  const filePath = join(context.globalStorageUri.fsPath, "note");
+  await writeFile(filePath, input);
+  vscode.window.showInformationMessage(`add note: ${input}`);
 }
