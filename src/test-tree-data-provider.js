9a5bd499ce2360eadbb2d4c04f39f2730cbb47e5
Conut-1 <1mim1@naver.com>
2025-01-01 18:17:58 +0900

    docs: JSDoc 추가

--- src/test-tree-data-provider.js
+++ src/test-tree-data-provider.js
@@ -2,13 +2,22 @@ import vscode from "./vscode-module.js";
 import { join } from "path";
 import { readFile } from "fs/promises";
 
+/**
+ * @implements {vscode.TreeDataProvider}
+ */
 export class TestTreeDataProvider {
+  /**
+   * @param {vscode.ExtensionContext} context
+   */
   constructor(context) {
     this._onDidChangeTreeData = new vscode.EventEmitter();
     this.onDidChangeTreeData = this._onDidChangeTreeData.event;
     this.context = context;
   }
 
+  /**
+   * @returns {vscode.TreeItem}
+   */
   getTreeItem(element) {
     return element;
   }
